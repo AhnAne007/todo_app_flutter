d25bb9ea4fc487f484947966e58d1c37f1725aeb
AhnAne007 <ahnaf.anis@gmail.com>
2023-04-25 19:36:18 +0500

    UI: Completed HomePage UI

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:intl/intl.dart';
 import 'package:to_do_app/resources/db_helper.dart';
 import 'package:to_do_app/view/screens/add_update_screen.dart';
 
 import '../../model/todo.dart';
+
+enum Actions { done, notDone, delete }
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,10 +23,6 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     dbHelper = DBHelper();
-    loadData();
-  }
-
-  loadData() async {
     todoList = dbHelper!.getListOfTodos();
   }
 
@@ -53,9 +53,17 @@ class _HomePageState extends State<HomePage> {
               child: FutureBuilder(
             future: todoList,
             builder: (context, AsyncSnapshot<List<Todo>> snapshot) {
-              if (!snapshot.hasData || snapshot.data == null) {
+              if (!snapshot.hasData) {
+                print(todoList);
                 return Center(
-                  child: CircularProgressIndicator(),
+                  child: Text(
+                    "No Tasks Found",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black,
+                    ),
+                  ),
                 );
               } else if (snapshot.data!.length == 0) {
                 return Center(
@@ -80,65 +88,131 @@ class _HomePageState extends State<HomePage> {
                     String toDoDateAndTime =
                         snapshot.data![index].dateAndTime.toString();
                     int toDoStatus = snapshot.data![index].status!.toInt();
-                    return Container(
-                      margin: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: toDoStatus == 0
-                            ? Colors.yellow.shade300
-                            : Colors.lightGreenAccent,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 4,
-                            spreadRadius: 1,
+                    return Slidable(
+                      startActionPane: ActionPane(
+                        motion: const StretchMotion(),
+                        children: [
+                          SlidableAction(
+                            onPressed: (context) {
+                              if (toDoStatus == 0) {
+                                setState(() {
+                                  dbHelper!.update(Todo(
+                                      id: toDoId,
+                                      title: toDoTitle,
+                                      description: toDoDescription,
+                                      status: 1,
+                                      dateAndTime: DateFormat('yMd')
+                                          .add_jm()
+                                          .format(DateTime.now())
+                                          .toString()));
+                                  todoList = dbHelper!.getListOfTodos();
+                                });
+                              } else {
+                                setState(() {
+                                  dbHelper!.update(Todo(
+                                      id: toDoId,
+                                      title: toDoTitle,
+                                      description: toDoDescription,
+                                      status: 0,
+                                      dateAndTime: DateFormat('yMd')
+                                          .add_jm()
+                                          .format(DateTime.now())
+                                          .toString()));
+                                  todoList = dbHelper!.getListOfTodos();
+                                });
+                              }
+                            },
+                            backgroundColor: toDoStatus == 0
+                                ? Colors.green
+                                : Colors.amber.shade900,
+                            icon: toDoStatus == 0 ? Icons.check : Icons.close,
+                            label: toDoStatus == 0
+                                ? 'Mark as Done'
+                                : 'Task Not Completed',
                           )
                         ],
                       ),
-                      child: Column(
+                      endActionPane: ActionPane(
+                        motion: const BehindMotion(),
                         children: [
-                          ListTile(
-                            contentPadding: EdgeInsets.all(10),
-                            title: Padding(
-                              padding: EdgeInsets.only(bottom: 10),
-                              child: Text(
-                                toDoTitle,
-                                style: TextStyle(fontSize: 19),
+                          SlidableAction(
+                            onPressed: (context) {
+                              setState(() {
+                                dbHelper!.delete(toDoId);
+                                todoList = dbHelper!.getListOfTodos();
+                                snapshot.data!.remove(snapshot.data![index]);
+                              });
+                            },
+                            backgroundColor: Colors.redAccent,
+                            icon: Icons.delete,
+                            label: 'Delete the Task',
+                          ),
+                        ],
+                      ),
+                      child: Container(
+                        margin: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: toDoStatus == 0
+                              ? Colors.yellow.shade300
+                              : Colors.lightGreenAccent,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 4,
+                              spreadRadius: 1,
+                            )
+                          ],
+                        ),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              contentPadding: EdgeInsets.all(10),
+                              title: Padding(
+                                padding: EdgeInsets.only(bottom: 10),
+                                child: Text(
+                                  toDoTitle,
+                                  style: TextStyle(fontSize: 19),
+                                ),
+                              ),
+                              subtitle: Text(
+                                toDoDescription,
+                                style: TextStyle(fontSize: 17),
                               ),
                             ),
-                            subtitle: Text(
-                              toDoDescription,
-                              style: TextStyle(fontSize: 17),
+                            Divider(
+                              color: Colors.black,
                             ),
-                          ),
-                          Divider(
-                            color: Colors.black,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 3),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Text(
-                                  toDoDateAndTime,
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
-                                    fontStyle: FontStyle.italic,
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 3),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    toDoDateAndTime,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.italic,
+                                    ),
                                   ),
-                                ),
-                                InkWell(
-                                  onTap: (){},
-                                  child: Icon(
-                                    Icons.edit_note,
-                                    color: Colors.deepPurpleAccent,
-                                    size: 30,
+                                  SizedBox(
+                                    width: 100,
                                   ),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
+                                  InkWell(
+                                    onTap: () {},
+                                    child: Icon(
+                                      Icons.edit_note,
+                                      color: Colors.deepOrange,
+                                      size: 30,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     );
                   },
@@ -162,4 +236,6 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  onDissmissed(int index, Actions actions) {}
 }
